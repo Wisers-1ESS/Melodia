8da15e0304030e5a87bef6d3f7a1f3501addca17
Mats057 <119885089+Mats057@users.noreply.github.com>
2024-10-05 19:09:23 -0300

    hero

---
+++ index.js
@@ -0,0 +1,9 @@
+const header = document.querySelector('header');
+
+document.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+    });
